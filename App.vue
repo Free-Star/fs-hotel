b20f5a9c2f1f48c21423fe9b2968d9a3684ca983
Free-Star <77061603+Free-Star@users.noreply.github.com>
2025-06-26 14:03:30 +0800

    添加 package.json 及启动说明

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="content">
+    <slot />
+  </view>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
---
+++ pages/contact/contact.vue
@@ -0,0 +1,49 @@
+<template>
+  <view class="page">
+    <view class="info">
+      <view class="label">微信号：</view>
+      <view class="value" @click="copyWx">{{ wechat }}</view>
+    </view>
+    <button @click="callPhone">拨打电话</button>
+    <image src="/static/img/qrcode.jpg" class="qrcode" mode="widthFix" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      wechat: 'hotel123',
+      phone: '13800138000'
+    }
+  },
+  methods: {
+    copyWx() {
+      uni.setClipboardData({ data: this.wechat })
+    },
+    callPhone() {
+      uni.makePhoneCall({ phoneNumber: this.phone })
+    }
+  }
+}
+</script>
+
+<style>
+.page {
+  padding: 20rpx;
+}
+.info {
+  display: flex;
+  margin-bottom: 20rpx;
+}
+.label {
+  width: 150rpx;
+}
+.value {
+  color: #007AFF;
+}
+.qrcode {
+  width: 200rpx;
+  margin-top: 20rpx;
+}
+</style>
