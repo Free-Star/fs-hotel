0e3dfa2b7538ee4c8c9312a95f51d68809ed75b0
Free-Star <77061603+Free-Star@users.noreply.github.com>
2025-06-26 13:51:50 +0800

    feat: init uniapp skeleton

---
+++ pages/about/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="page">
+    <view class="title">关于我们</view>
+    <view class="text">本民宿位于美丽的海边，提供温馨舒适的住宿体验。</view>
+    <view class="contact">微信：hotel123</view>
+    <view class="contact">电话：13800138000</view>
+  </view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.page {
+  padding: 20rpx;
+}
+.title {
+  font-size: 32rpx;
+  font-weight: bold;
+  margin-bottom: 10rpx;
+}
+.text {
+  margin-bottom: 10rpx;
+  line-height: 1.5;
+}
+.contact {
+  font-size: 24rpx;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <view class="page">
+    <!-- Banner -->
+    <swiper autoplay circular class="banner">
+      <swiper-item v-for="(item, index) in banners" :key="index">
+        <image :src="item" class="banner-image" />
+      </swiper-item>
+    </swiper>
+
+    <!-- 推荐房源 -->
+    <view class="recommend">
+      <view class="title">推荐房源</view>
+      <view class="list">
+        <view class="card" v-for="(room, index) in recommends" :key="index" @click="toDetail(room)">
+          <image :src="room.img" class="card-image" />
+          <view class="card-name">{{ room.name }}</view>
+          <view class="card-desc">{{ room.desc }}</view>
+        </view>
+      </view>
+    </view>
+
+    <button class="all-btn" @click="toList">查看全部房源</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      banners: [
+        '/static/img/banner1.jpg',
+        '/static/img/banner2.jpg',
+        '/static/img/banner3.jpg'
+      ],
+      recommends: [
+        { img: '/static/img/r1.jpg', name: '房源A', desc: '舒适大床房' },
+        { img: '/static/img/r2.jpg', name: '房源B', desc: '海景套房' }
+      ]
+    }
+  },
+  methods: {
+    toList() {
+      uni.navigateTo({ url: '/pages/list/list' })
+    },
+    toDetail(room) {
+      uni.navigateTo({ url: '/pages/detail/detail?name=' + room.name })
+    }
+  }
+}
+</script>
+
+<style>
+.banner {
+  height: 200rpx;
+}
+.banner-image {
+  width: 100%;
+  height: 200rpx;
+}
+.recommend {
+  padding: 20rpx;
+}
+.title {
+  font-weight: bold;
+  margin-bottom: 10rpx;
+}
+.list {
+  display: flex;
+}
+.card {
+  width: 45%;
+  margin-right: 5%;
+}
+.card-image {
+  width: 100%;
+  height: 150rpx;
+}
+.card-name {
+  font-size: 28rpx;
+}
+.card-desc {
+  font-size: 24rpx;
+  color: #666;
+}
+.all-btn {
+  margin: 20rpx;
+}
+</style>
